02d22430bf971221626036c6b81e2efae6c3f042
AaryadevG <44947267+aaryadevg@users.noreply.github.com>
2021-09-19 12:49:58 +0200

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -2,6 +2,7 @@ import requests
 
 # TODO: Check prediction with all files
 # TODO: Put PyTest maybe?
+TEST_FILE = "OtherSize.png"
 URL = "http://127.0.0.1:5000/predict"
-resp = requests.post(URL, files={"Image" : open("OtherSize.png", "rb")})
+resp = requests.post(URL, files={"Image" : open(TEST_FILE, "rb")})
 print(resp.text)
